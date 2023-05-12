c6cd5c257f98d30b23fe290a3b884d24adb16364
Anas Hamo <anas.hamo.023@gmail.com>
2023-05-12 23:39:21 +0300

    up css file

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,44 @@
 @extends('layouts.app')
-
-
-
 @section('content')
-    <div class="pt-32">
-          <h1 class="main-title-a">حول</h1>
+    <div class="py-9">
+        <div class="bg1-a about-head-a">
+            <h1 class="main-title-a">حول</h1>
+        </div>
+          <div class="team " id="team">
+            <h2 class="main-title-a ">فريق العمل</h2>
+
+            <div class="container ">
+                <div class="box ">
+                    <div class="data ">
+                        <img src="{{'img/1.png'}}" alt=" ">
+                        <div class="social ">
+                            <a href="# "><i class="fab fa-facebook-f "></i></a>
+                            <a href="# "><i class="fab fa-twitter "></i></a>
+                            <a href="# "><i class="fab fa-linkedin-in "></i></a>
+                            <a href="# "><i class="fab fa-youtube "></i></a>
+                        </div>
+                    </div>
+                    <div class="info ">
+                        <h3>Wesal Husien</h3>
+                        <p>Simple Short Description</p>
+                    </div>
+                </div>
+                <div class="box ">
+                    <div class="data ">
+                        <img src="{{'img/2.png'}}" alt=" ">
+                        <div class="social ">
+                            <a href="# "><i class="fab fa-facebook-f "></i></a>
+                            <a href="# "><i class="fab fa-twitter "></i></a>
+                            <a href="# "><i class="fab fa-linkedin-in "></i></a>
+                            <a href="# "><i class="fab fa-youtube "></i></a>
+                        </div>
+                    </div>
+                    <div class="info ">
+                        <h3>Wesal Husien</h3>
+                        <p>Simple Short Description</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link href="{{ url('css/our.css') }}" rel="stylesheet">
+    <link href="{{ url('css/contact.css') }}" rel="stylesheet">
+    <link href="{{ url('css/footer.css') }}" rel="stylesheet">
+    <link href="{{ url('css/about.css') }}" rel="stylesheet">
+
 </head>
 <body class="bg-white h-screen antialiased leading-none font-sans">
     <div id="app">
